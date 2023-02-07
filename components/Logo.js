4e0f2d4d590795becb4128c1ec9529d8e98292c5
Marvin Kome <marvinkome@gmail.com>
2023-02-07 20:59:19 +0100

    add form step-1

--- components/Logo.js
+++ components/Logo.js
@@ -2,19 +2,8 @@ import { Heading, chakra } from "@chakra-ui/react";
 
 export default function Logo() {
   return (
-    <chakra.div
-      px={5}
-      py={1}
-      borderTopEndRadius="md"
-      borderBottomEndRadius="md"
-      bgColor="grey"
-    >
-      <Heading
-        alignSelf="center"
-        pl={1}
-        color="rgb(255 255 255 / 95%)"
-        fontSize="2xl"
-      >
+    <chakra.div px={5} py={1} borderTopEndRadius="md" borderBottomEndRadius="md" bgColor="grey" w="auto">
+      <Heading alignSelf="center" pl={1} color="rgb(255 255 255 / 95%)" fontSize="2xl">
         T<chakra.span color="tasker_green.300">ask</chakra.span>er
         <chakra.span color="tasker_green.300">Now</chakra.span>
       </Heading>
